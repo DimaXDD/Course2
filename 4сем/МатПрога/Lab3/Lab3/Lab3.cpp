6a24aaba2bb56858b8ecd75cbd26d6ef498af400
DimaXDD <dimatruba2004@yandex.ru>
2023-03-01 01:54:32 +0300

    Lab3 МатПрога full

---
+++ 4сем/МатПрога/Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include "Salesman.h"
+#define N 5
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int d[N][N] = { //0   1    2    3     4        
+                  { INF, 22, 32, INF, 11},    //  0
+                  { 11, INF,  26,  57, 73},    //  1
+                  { 13,  33, INF, 86, 60},    //  2 
+                  { 28,  47,  44, INF, 33},    //  3
+                  { 82,  77,  52,  24, INF} };   //  4  
+    int r[N];                     // результат 
+    int s = salesman(
+        N,          // [in]  количество городов 
+        (int*)d,          // [in]  массив [n*n] расстояний 
+        r           // [out] массив [n] маршрут 0 x x x x  
+
+    );
+    std::cout << std::endl << "-- Задача коммивояжера -- ";
+    std::cout << std::endl << "-- количество  городов: " << N;
+    std::cout << std::endl << "-- матрица расстояний : ";
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << std::endl;
+        for (int j = 0; j < N; j++)
+
+            if (d[i][j] != INF) std::cout << std::setw(3) << d[i][j] << " ";
+
+            else std::cout << std::setw(3) << "INF" << " ";
+    }
+    std::cout << std::endl << "-- оптимальный маршрут: ";
+    for (int i = 0; i < N; i++) std::cout << r[i] << "-->"; std::cout << 0;
+    std::cout << std::endl << "-- длина маршрута     : " << s;
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
+
+
+//#include "Auxil.h"
+//#include <iostream>
+//#include <iomanip> 
+//#include <time.h>
+//#include "Salesman.h"
+//#define SPACE(n) std::setw(n)<<" "
+//#define N 12
+//int main()
+//{
+//    setlocale(LC_ALL, "rus");
+//    int d[N * N + 1], r[N];
+//    auxil::start();
+//    for (int i = 0; i <= N * N; i++) d[i] = auxil::iget(10, 100);
+//    std::cout << std::endl << "-- Задача коммивояжера -- ";
+//    std::cout << std::endl << "-- количество ------ продолжительность -- ";
+//    std::cout << std::endl << "      городов           вычисления  ";
+//    clock_t t1, t2;
+//    for (int i = 7; i <= N; i++)
+//    {
+//        t1 = clock();
+//        salesman(i, (int*)d, r);
+//        t2 = clock();
+//        std::cout << std::endl << SPACE(7) << std::setw(2) << i
+//            << SPACE(15) << std::setw(5) << (t2 - t1);
+//    }
+//    std::cout << std::endl;
+//    system("pause");
+//    return 0;
+//}
+
