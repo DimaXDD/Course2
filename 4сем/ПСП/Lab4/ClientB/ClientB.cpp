2ceef796fc6175ba59693e0c02804cb5fc219ecd
DimaXDD <dimatruba2004@yandex.ru>
2023-03-07 02:05:11 +0300

    Lab4 ПСП

---
+++ 4сем/ПСП/Lab4/ClientB/ClientB.cpp
@@ -0,0 +1,281 @@
+﻿#include <iostream>
+#include <string>
+#include "Winsock2.h"
+#include <Windows.h>
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma warming(disable:4996)
+
+using namespace std;
+
+SOCKET clientSocket;
+
+string GetErrorMsgText(int code)
+{
+	string msgText;
+	switch (code)
+	{
+	case WSAEINTR:
+		msgText = "Function interrupted";
+		break;
+	case WSAEACCES:
+		msgText = "Permission denied";
+		break;
+	case WSAEFAULT:
+		msgText = "Wrong address";
+		break;
+	case WSAEINVAL:
+		msgText = "Wrong args";
+		break;
+	case WSAEMFILE:
+		msgText = "Too many files have opened";
+		break;
+	case WSAEWOULDBLOCK:
+		msgText = "Resource temporarily unavailable";
+		break;
+	case WSAEINPROGRESS:
+		msgText = "Operation in progress";
+		break;
+	case WSAEALREADY:
+		msgText = "Operation already in progress";
+		break;
+	case WSAENOTSOCK:
+		msgText = "Wrong socket";
+		break;
+	case WSAEDESTADDRREQ:
+		msgText = "Required adress location";
+		break;
+	case WSAEMSGSIZE:
+		msgText = "Message is too long ";
+		break;
+	case WSAEPROTOTYPE:
+		msgText = "Wrong protocol type to socket";
+		break;
+	case WSAENOPROTOOPT:
+		msgText = "Error in protocol option";
+		break;
+	case WSAEPROTONOSUPPORT:
+		msgText = "Protocol is not supported";
+		break;
+	case WSAESOCKTNOSUPPORT:
+		msgText = "Socket type is not supported";
+		break;
+	case WSAEOPNOTSUPP:
+		msgText = "Operation is not supported";
+		break;
+	case WSAEPFNOSUPPORT:
+		msgText = "Protocol type is not supported";
+		break;
+	case WSAEAFNOSUPPORT:
+		msgText = "Addresses type is not supported by protocol";
+		break;
+	case WSAEADDRINUSE:
+		msgText = "Address is already used";
+		break;
+	case WSAEADDRNOTAVAIL:
+		msgText = "Requested address cannot be used";
+		break;
+	case WSAENETDOWN:
+		msgText = "Network disconnected";
+		break;
+	case WSAENETUNREACH:
+		msgText = "Network is unttainable";
+		break;
+	case WSAENETRESET:
+		msgText = "Network dropped the connection";
+		break;
+	case WSAECONNABORTED:
+		msgText = "Connection aborted";
+		break;
+	case WSAECONNRESET:
+		msgText = "Connection restored";
+		break;
+	case WSAENOBUFS:
+		msgText = "Not enough memory for buffers";
+		break;
+	case WSAEISCONN:
+		msgText = "Socket has already connected";
+		break;
+	case WSAENOTCONN:
+		msgText = "Socket has not connected";
+		break;
+	case WSAESHUTDOWN:
+		msgText = "Send cannot be performed: socket was shutdowned";
+		break;
+	case WSAETIMEDOUT:
+		msgText = "Alloted time interval has ended";
+		break;
+	case WSAECONNREFUSED:
+		msgText = "Connection was rejected";
+		break;
+	case WSAEHOSTDOWN:
+		msgText = "Host was shotdowned";
+		break;
+	case WSAEHOSTUNREACH:
+		msgText = "Has not way for host";
+		break;
+	case WSAEPROCLIM:
+		msgText = "Too many processes";
+		break;
+	case WSASYSNOTREADY:
+		msgText = "Network is unavailable";
+		break;
+	case WSAVERNOTSUPPORTED:
+		msgText = "Version is not supported";
+		break;
+	case WSANOTINITIALISED:
+		msgText = "WS2_32.dll is not initialised";
+		break;
+	case WSAEDISCON:
+		msgText = "Connection in progress";
+		break;
+	case WSATYPE_NOT_FOUND:
+		msgText = "Type is not found";
+		break;
+	case WSAHOST_NOT_FOUND:
+		msgText = "Host is not found";
+		break;
+	case WSATRY_AGAIN:
+		msgText = "Try again";
+		break;
+	case WSANO_RECOVERY:
+		msgText = "Unknown error";
+		break;
+	case WSANO_DATA:
+		msgText = "Has not data type";
+		break;
+	case WSA_INVALID_HANDLE:
+		msgText = "Invalid handle";
+		break;
+	case WSA_INVALID_PARAMETER:
+		msgText = "Invalid one or many parameters";
+		break;
+	case WSA_IO_INCOMPLETE:
+		msgText = "IO object is incomplete";
+		break;
+	case WSA_IO_PENDING:
+		msgText = "Operation will end later";
+		break;
+	case WSA_NOT_ENOUGH_MEMORY:
+		msgText = "Not enough memory";
+		break;
+	case WSA_OPERATION_ABORTED:
+		msgText = "Operation was aborted";
+		break;
+	case WSAEINVALIDPROCTABLE:
+		msgText = "Invalid provider";
+		break;
+	case WSAEINVALIDPROVIDER:
+		msgText = "Error in provider version";
+		break;
+	case WSAEPROVIDERFAILEDINIT:
+		msgText = "Failed provider initialization";
+		break;
+	case WSASYSCALLFAILURE:
+		msgText = "Abnormal termination of a system call";
+		break;
+	default:
+		msgText = "Unknown error";
+		break;
+	}
+	return msgText;
+}
+
+string SetErrorMsgText(string msgText, int code)
+{
+	return msgText + GetErrorMsgText(code);
+}
+
+bool GetServer(char* call, short port, sockaddr* from, int* flen)
+{
+	memset(from, 0, sizeof(flen));
+
+	if ((clientSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+	{
+		throw  SetErrorMsgText("socket:", WSAGetLastError());
+	}
+
+	int optval = 1;														// состояние enabled
+								//установить опции сокета
+	if (setsockopt(clientSocket,
+		SOL_SOCKET,														// уровень д-вия режима
+		SO_BROADCAST,													// режим сокета для исп-ния широковещ.адреса
+		(char*)&optval,													// значение режима сокета
+		sizeof(int)) == SOCKET_ERROR)									// длина буфера оптвал
+	{
+		throw  SetErrorMsgText("opt:", WSAGetLastError());
+	}
+
+	SOCKADDR_IN all;
+	all.sin_family = AF_INET;
+	all.sin_port = htons(2000);
+	all.sin_addr.s_addr = INADDR_BROADCAST;
+
+	if ((sendto(clientSocket, call, strlen(call) + 1, NULL, (sockaddr*)&all, sizeof(all))) == SOCKET_ERROR)
+	{
+		throw  SetErrorMsgText("sendto:", WSAGetLastError());
+	}
+
+	char nameServer[50];
+	if ((recvfrom(clientSocket, nameServer, sizeof(nameServer), NULL, from, flen)) == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() == WSAETIMEDOUT)
+		{
+			return false;
+		}
+		else
+		{
+			throw  SetErrorMsgText("recv:", WSAGetLastError());
+		}
+	}
+
+	SOCKADDR_IN* addr = (SOCKADDR_IN*)&from;
+	std::cout << std::endl << "Порт сервера: " << addr->sin_port;
+	std::cout << std::endl << "IP-адрес сервера: " << inet_ntoa(addr->sin_addr);
+
+	if (!strcmp(nameServer, call))
+	{
+		std::cout << std::endl << "Сервер с таким именем найден.";
+		return true;
+	}
+	else
+	{
+		std::cout << std::endl << "Сервер с таким именем не найден.";
+		return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	try {
+
+		WSADATA wsaData;
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+		{
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+		}
+
+		char call[] = "Hello";
+
+		SOCKADDR_IN clnt;
+		int lc = sizeof(clnt);
+
+		GetServer(call, 2000, (sockaddr*)&clnt, &lc);
+
+		if (closesocket(clientSocket) == SOCKET_ERROR)
+		{
+			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+		}
+		if (WSACleanup() == SOCKET_ERROR)
+		{
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+		}
+
+	}
+	catch (string errorMsgText) {
+		cout << endl << "WSAGetLastError: " << errorMsgText; 
+	}
+}
---
+++ 4сем/ПСП/Lab4/ServerB/ServerB.cpp
@@ -0,0 +1,324 @@
+﻿#include <iostream>
+#include <string>
+#include "Winsock2.h"
+#include <Windows.h>
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma warming(disable:4996)
+
+using namespace std;
+
+SOCKET serverSocket;
+char name[] = "Hello";					// позывной сервера
+char checkServName[] = "CheckServ"; 	// позывной клиента
+
+string GetErrorMsgText(int code)
+{
+	string msgText;
+	switch (code)
+	{
+	case WSAEINTR:
+		msgText = "Function interrupted";
+		break;
+	case WSAEACCES:
+		msgText = "Permission denied";
+		break;
+	case WSAEFAULT:
+		msgText = "Wrong address";
+		break;
+	case WSAEINVAL:
+		msgText = "Wrong args";
+		break;
+	case WSAEMFILE:
+		msgText = "Too many files have opened";
+		break;
+	case WSAEWOULDBLOCK:
+		msgText = "Resource temporarily unavailable";
+		break;
+	case WSAEINPROGRESS:
+		msgText = "Operation in progress";
+		break;
+	case WSAEALREADY:
+		msgText = "Operation already in progress";
+		break;
+	case WSAENOTSOCK:
+		msgText = "Wrong socket";
+		break;
+	case WSAEDESTADDRREQ:
+		msgText = "Required adress location";
+		break;
+	case WSAEMSGSIZE:
+		msgText = "Message is too long ";
+		break;
+	case WSAEPROTOTYPE:
+		msgText = "Wrong protocol type to socket";
+		break;
+	case WSAENOPROTOOPT:
+		msgText = "Error in protocol option";
+		break;
+	case WSAEPROTONOSUPPORT:
+		msgText = "Protocol is not supported";
+		break;
+	case WSAESOCKTNOSUPPORT:
+		msgText = "Socket type is not supported";
+		break;
+	case WSAEOPNOTSUPP:
+		msgText = "Operation is not supported";
+		break;
+	case WSAEPFNOSUPPORT:
+		msgText = "Protocol type is not supported";
+		break;
+	case WSAEAFNOSUPPORT:
+		msgText = "Addresses type is not supported by protocol";
+		break;
+	case WSAEADDRINUSE:
+		msgText = "Address is already used";
+		break;
+	case WSAEADDRNOTAVAIL:
+		msgText = "Requested address cannot be used";
+		break;
+	case WSAENETDOWN:
+		msgText = "Network disconnected";
+		break;
+	case WSAENETUNREACH:
+		msgText = "Network is unttainable";
+		break;
+	case WSAENETRESET:
+		msgText = "Network dropped the connection";
+		break;
+	case WSAECONNABORTED:
+		msgText = "Connection aborted";
+		break;
+	case WSAECONNRESET:
+		msgText = "Connection restored";
+		break;
+	case WSAENOBUFS:
+		msgText = "Not enough memory for buffers";
+		break;
+	case WSAEISCONN:
+		msgText = "Socket has already connected";
+		break;
+	case WSAENOTCONN:
+		msgText = "Socket has not connected";
+		break;
+	case WSAESHUTDOWN:
+		msgText = "Send cannot be performed: socket was shutdowned";
+		break;
+	case WSAETIMEDOUT:
+		msgText = "Alloted time interval has ended";
+		break;
+	case WSAECONNREFUSED:
+		msgText = "Connection was rejected";
+		break;
+	case WSAEHOSTDOWN:
+		msgText = "Host was shotdowned";
+		break;
+	case WSAEHOSTUNREACH:
+		msgText = "Has not way for host";
+		break;
+	case WSAEPROCLIM:
+		msgText = "Too many processes";
+		break;
+	case WSASYSNOTREADY:
+		msgText = "Network is unavailable";
+		break;
+	case WSAVERNOTSUPPORTED:
+		msgText = "Version is not supported";
+		break;
+	case WSANOTINITIALISED:
+		msgText = "WS2_32.dll is not initialised";
+		break;
+	case WSAEDISCON:
+		msgText = "Connection in progress";
+		break;
+	case WSATYPE_NOT_FOUND:
+		msgText = "Type is not found";
+		break;
+	case WSAHOST_NOT_FOUND:
+		msgText = "Host is not found";
+		break;
+	case WSATRY_AGAIN:
+		msgText = "Try again";
+		break;
+	case WSANO_RECOVERY:
+		msgText = "Unknown error";
+		break;
+	case WSANO_DATA:
+		msgText = "Has not data type";
+		break;
+	case WSA_INVALID_HANDLE:
+		msgText = "Invalid handle";
+		break;
+	case WSA_INVALID_PARAMETER:
+		msgText = "Invalid one or many parameters";
+		break;
+	case WSA_IO_INCOMPLETE:
+		msgText = "IO object is incomplete";
+		break;
+	case WSA_IO_PENDING:
+		msgText = "Operation will end later";
+		break;
+	case WSA_NOT_ENOUGH_MEMORY:
+		msgText = "Not enough memory";
+		break;
+	case WSA_OPERATION_ABORTED:
+		msgText = "Operation was aborted";
+		break;
+	case WSAEINVALIDPROCTABLE:
+		msgText = "Invalid provider";
+		break;
+	case WSAEINVALIDPROVIDER:
+		msgText = "Error in provider version";
+		break;
+	case WSAEPROVIDERFAILEDINIT:
+		msgText = "Failed provider initialization";
+		break;
+	case WSASYSCALLFAILURE:
+		msgText = "Abnormal termination of a system call";
+		break;
+	default:
+		msgText = "Unknown error";
+		break;
+	}
+	return msgText;
+}
+
+string SetErrorMsgText(string msgText, int code)
+{
+	return msgText + GetErrorMsgText(code);
+}
+
+bool GetRequestFromClient(char* name, short port, sockaddr* from, int* flen)
+{
+	char nameServer[50];				// имя сервера
+	memset(from, 0, sizeof(flen));		// очистка структуры
+
+	if ((serverSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)	// для сетвеого протокола ipv4, датаграмнный сокет (поток)
+	{
+		throw  SetErrorMsgText("socket:", WSAGetLastError());
+	}
+
+	SOCKADDR_IN serv;
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(port);
+	serv.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(serverSocket, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR)	// связывание сокета с адресом
+	{
+		throw  SetErrorMsgText("Походу такой сервер уже есть: ", WSAGetLastError());
+	}
+
+	if ((recvfrom(serverSocket, nameServer, sizeof(nameServer), NULL, from, flen)) == SOCKET_ERROR) // ожидание запроса
+	{
+		if (WSAGetLastError() == WSAETIMEDOUT)
+		{
+			return false;
+		}
+		else
+		{
+			throw  SetErrorMsgText("recv:", WSAGetLastError());
+		}
+	}
+
+	if (!strcmp(nameServer, name)) // проверка на совпадение имени сервера
+	{
+		cout << endl << "Позывной сервера совпадает.";
+		return true;
+	}
+	else if (strcmp(nameServer, checkServName) == 0) {
+		cout << "Найден идентичный сервер!" << endl;
+		return false;
+	}
+	else
+	{
+		cout << endl << "Позывной сервера не совпадает.";
+		return false;
+	}
+}
+
+bool PutAnswerToClient(const char* name, sockaddr* to, int* lto)
+{
+	if ((sendto(serverSocket, name, strlen(name) + 1, NULL, to, *lto)) == SOCKET_ERROR)
+	{
+		throw  SetErrorMsgText("send:", WSAGetLastError());
+	}
+
+	return true;
+}
+
+void CheckServer() {
+
+	char checkServ[] = "CheckServ";
+
+	int checkServer;
+	if ((checkServer = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+		throw  SetErrorMsgText("socket:", WSAGetLastError());
+
+	int optval = 1;										// equals to 'enabled'			
+
+	if (setsockopt(checkServer, SOL_SOCKET,				// уровень д-вия режима
+		SO_BROADCAST,									// режим сокета для исп-ния широковещ.адреса
+		(char*)&optval,									// значение режима сокета
+		sizeof(int)) == SOCKET_ERROR)					// длина буфера оптвал
+	{
+		throw  SetErrorMsgText("opt:", WSAGetLastError());
+	}
+
+	SOCKADDR_IN all;
+	all.sin_family = AF_INET;
+	all.sin_port = htons(2000);
+	all.sin_addr.s_addr = INADDR_BROADCAST;
+
+	if ((sendto(checkServer, checkServ, strlen(checkServ) + 1, NULL, (sockaddr*)&all, sizeof(all))) == SOCKET_ERROR)
+		throw  SetErrorMsgText("sendto:", WSAGetLastError());
+
+	if (closesocket(checkServer) == SOCKET_ERROR)
+		throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	WSADATA wsaData;
+
+	try
+	{
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)								// инициализация библиотеки
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+
+		CheckServer();
+
+		while (true) {
+			SOCKADDR_IN clnt;
+			int lc = sizeof(clnt);
+
+			if (GetRequestFromClient(name, 2000, (sockaddr*)&clnt, &lc))
+			{
+				std::cout << std::endl << PutAnswerToClient(name, (sockaddr*)&clnt, &lc);
+			}
+			else
+			{
+				std::cout << std::endl << PutAnswerToClient("*Error name*", (sockaddr*)&clnt, &lc);
+			}
+
+			SOCKADDR_IN* addr = (SOCKADDR_IN*)&clnt;
+			std::cout << std::endl << "Порт клиента: " << addr->sin_port;
+			std::cout << std::endl << "IP-адрес клиента: " << inet_ntoa(addr->sin_addr);
+
+			if (closesocket(serverSocket) == SOCKET_ERROR)							// закрыть сокет
+				throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+		}
+
+		if (WSACleanup() == SOCKET_ERROR)											// завершить работу с библиотекой
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+	}
+	catch (string errorMsgText)
+	{
+		cout << endl << "WSAGetLastError: " << errorMsgText;
+	}
+
+	system("pause");
+	return 0;
+}
+
+
