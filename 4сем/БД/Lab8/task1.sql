21fe80f1bb5cf16db41a7e1844239ccb62855ed0
DimaXDD <dimatruba2004@yandex.ru>
2023-04-06 12:30:28 +0300

    Refactor Lab8 ПСП and more

--- 4сем/БД/Lab8/task1.sql
+++ 4сем/БД/Lab8/task1.sql
@@ -6,6 +6,15 @@ as
 select 
 GENDER[Пол], PULPIT[Кафедра], TEACHER[Код], TEACHER_NAME[Имя] from TEACHER;
 
+
+UPDATE TEACHER
+SET GENDER = 'м'
+WHERE TEACHER_NAME = 'Рожков Леонид Николаевич';
+
+delete from TEACHER
+WHERE TEACHER_NAME = 'Рожков Леонид Николаевич';
+
+
 go
 select * from [Преподаватель]
 
--- 4сем/БД/Lab8/task5.sql
+++ 4сем/БД/Lab8/task5.sql
@@ -6,7 +6,9 @@ as
 select TOP 100 SUBJECT[Код], SUBJECT_NAME[Наименование дисциплины], PULPIT[Кафедра] 
 from SUBJECT order by SUBJECT.SUBJECT_NAME
 
-go
+insert Дисциплины values('БД1', 'Не бд1', 'ИСиТ')
+
+go	
 select * from [Дисциплины]
 
 go
