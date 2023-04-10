50b71a1a3f2175b8e891778c3daba7c665bca961
DimaXDD <dimatruba2004@yandex.ru>
2023-04-11 01:35:11 +0300

    Lab8 ПСП первая попытка

---
+++ 4сем/ПСП/Lab8/ConcurrentServer/ErrorFunctions.cpp
@@ -0,0 +1,76 @@
+﻿#include "ErrorFunctions.h"
+
+string  GetErrorMsgText(int code)
+{
+	string msgText;
+	switch (code)
+	{
+	case WSAEINTR:          msgText = "WSAEINTR";         break;
+	case WSAEACCES:         msgText = "WSAEACCES";        break;
+
+	case WSAEFAULT:          msgText = "WSAEFAULT";         break;
+	case WSAEINVAL:         msgText = "WSAEINVAL";        break;
+	case WSAEMFILE:          msgText = "WSAEMFILE";         break;
+	case WSAEWOULDBLOCK:         msgText = "WSAEWOULDBLOCK";        break;
+	case WSAEINPROGRESS:          msgText = "WSAEINPROGRESS";         break;
+	case WSAEALREADY:         msgText = "WSAEALREADY";        break;
+	case WSAENOTSOCK:          msgText = "WSAENOTSOCK";         break;
+	case WSAEDESTADDRREQ:         msgText = "WSAEDESTADDRREQ";        break;
+	case WSAEMSGSIZE:          msgText = "WSAEMSGSIZE";         break;
+	case WSAEPROTOTYPE:         msgText = "WSAEPROTOTYPE";        break;
+	case WSAENOPROTOOPT:          msgText = "WSAENOPROTOOPT";         break;
+	case WSAEPROTONOSUPPORT:         msgText = "WSAEPROTONOSUPPORT";        break;
+	case WSAESOCKTNOSUPPORT:          msgText = "WSAESOCKTNOSUPPORT";         break;
+	case WSAEOPNOTSUPP:         msgText = "WSAEOPNOTSUPP";        break;
+	case WSAEPFNOSUPPORT:          msgText = "WSAEPFNOSUPPORT";         break;
+	case WSAEAFNOSUPPORT:         msgText = "WSAEAFNOSUPPORT";        break;
+	case WSAEADDRINUSE:          msgText = "WSAEADDRINUSE";         break;
+	case WSAEADDRNOTAVAIL:         msgText = "WSAEADDRNOTAVAIL";        break;
+	case WSAENETDOWN:          msgText = "WSAENETDOWN";         break;
+	case WSAENETUNREACH:         msgText = "WSAENETUNREACH";        break;
+	case WSAENETRESET:          msgText = "WSAENETRESET";         break;
+	case WSAECONNABORTED:         msgText = "WSAECONNABORTED";        break;
+	case WSAECONNRESET:          msgText = "WSAECONNRESET";         break;
+	case WSAENOBUFS:         msgText = "WSAENOBUFS";        break;
+	case WSAEISCONN:          msgText = "WSAEISCONN";         break;
+	case WSAENOTCONN:         msgText = "WSAENOTCONN";        break;
+	case WSAESHUTDOWN:          msgText = "WSAESHUTDOWN";         break;
+	case WSAETIMEDOUT:         msgText = "WSAETIMEDOUT";        break;
+	case WSAECONNREFUSED:          msgText = "WSAECONNREFUSED";         break;
+	case WSAEHOSTDOWN:         msgText = "WSAEHOSTDOWN";        break;
+	case WSAEHOSTUNREACH:          msgText = "WSAEHOSTUNREACH";         break;
+	case WSAEPROCLIM:         msgText = "WSAEPROCLIM";        break;
+	case WSASYSNOTREADY:          msgText = "WSASYSNOTREADY";         break;
+	case WSAVERNOTSUPPORTED:         msgText = "WSAVERNOTSUPPORTED";        break;
+	case WSANOTINITIALISED:          msgText = "WSANOTINITIALISED";         break;
+	case WSAEDISCON:         msgText = "WSAEDISCON";        break;
+	case WSATYPE_NOT_FOUND:          msgText = "WSATYPE_NOT_FOUND";         break;
+	case WSAHOST_NOT_FOUND:         msgText = "WSAHOST_NOT_FOUND";        break;
+	case WSATRY_AGAIN:          msgText = "WSATRY_AGAIN";         break;
+	case WSANO_RECOVERY:         msgText = "WSANO_RECOVERY";        break;
+	case WSANO_DATA:          msgText = "WSANO_DATA";         break;
+	case WSA_INVALID_HANDLE:         msgText = "WSA_INVALID_HANDLE";        break;
+	case WSA_INVALID_PARAMETER:          msgText = "WSA_INVALID_PARAMETER";         break;
+	case WSA_IO_INCOMPLETE:         msgText = "WSA_IO_INCOMPLETE";        break;
+	case WSA_IO_PENDING:          msgText = "WSA_IO_PENDING";         break;
+	case WSA_NOT_ENOUGH_MEMORY:         msgText = "WSA_NOT_ENOUGH_MEMORY";        break;
+	case WSA_OPERATION_ABORTED:          msgText = "WSA_OPERATION_ABORTED";         break;
+	case WSAEINVALIDPROCTABLE:         msgText = "WSAEINVALIDPROCTABLE";        break;
+	case WSAEINVALIDPROVIDER:          msgText = "WSAEINVALIDPROVIDER";         break;
+	case WSAEPROVIDERFAILEDINIT:         msgText = "WSAEPROVIDERFAILEDINIT";        break;
+
+	case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE"; break;
+	default:                msgText = "***ERROR***";      break;
+	};
+	return msgText;
+};
+
+string  SetErrorMsgText(string msgText, int code)
+{
+	return  msgText + GetErrorMsgText(code);
+};
+
+string  SetPipeError(string msgText, int code)
+{
+	return  msgText + GetErrorMsgText(code);
+};
---
+++ 4сем/ПСП/Lab8/ConcurrentServer/ErrorFunctions.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Winsock2.h"
+#pragma comment(lib, "WS2_32.lib") 
+#include <string>
+
+using namespace std;
+
+string  GetErrorMsgText(int code);
+string  SetErrorMsgText(string msgText, int code);
+string  SetPipeError(string msgText, int code);
